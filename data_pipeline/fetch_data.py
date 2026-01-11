75ed709aeb13e932c6f47e99f95a14bf8a1d03b1
Sena Kotan <senakotan34@gmail.com>
2026-01-11 18:16:55 +0300

    Update fetch_data.py

--- data_pipeline/fetch_data.py
+++ data_pipeline/fetch_data.py
@@ -2,13 +2,11 @@ import pandas as pd
 from .bigquery_connection import client
 
 
-#BigQuery de hangi projede çalışacağımıza dair sabitleri tanımlıyoruz.
 PROJECT_ID = "tabii-469409"
 DATASET_ID = "anormaly_detection"
-TABLE_FQN  = f"{PROJECT_ID}.{DATASET_ID}.tabii_engagement_data"
+TABLE_FQN = f"{PROJECT_ID}.{DATASET_ID}.tabii_engagement_data"
 LOCATION = "US"
 
-#Günlük oturum sayısı ve toplam izleme süresini hesaplayan SQL sorgusu.
 SQL = f"""
 WITH base AS (
   SELECT
@@ -27,24 +25,22 @@ GROUP BY event_date
 ORDER BY event_date
 """
 
+
 def get_daily_engagements():
-    #Aggregation işlemini yapacak olan sorguyu istemci yardımıyla çalıştırıyoruz. Bize bir QueryJob objesi döndürüyor.
-    job = client.query(SQL, location=LOCATION) 
-    #Sorgunun bitmesi beklenir ve sonuçlar DataFrame e dönüştürülür.
-    df = job.result().to_dataframe(create_bqstorage_client=True) #create_bqstorage_client= daha hızlı veri transferi sağlar.
-    
-    #Veri tipi dönüşümlerini yapıyoruz.
+    job = client.query(SQL, location=LOCATION)
+    df = job.result().to_dataframe(create_bqstorage_client=True)
+
     df["event_date"] = pd.to_datetime(df["event_date"])
     df["oturum_sayisi"] = pd.to_numeric(df["oturum_sayisi"])
     df["toplam_izleme_suresi_dk"] = pd.to_numeric(df["toplam_izleme_suresi_dk"])
+
     return df
 
+
 if __name__ == "__main__":
-    #Fonksiyonu çağırdık: BigQuery ile bağlanıldı, aggregation işlemi uygulandı, DataFrame'e dönüştürüldü, veri tipi dönüşümleri yapıldı.
     df = get_daily_engagements()
-    print("Satır:", len(df)) #381 günlük veri 
+    print("Satır:", len(df))
     print(df.head())
-    
-    #Veri kullanılmak üzere csv ye kaydedildi.
-    output_path = "data/daily_engagements.csv"   
+
+    output_path = "data/daily_engagements.csv"
     df.to_csv(output_path, index=False, encoding="utf-8-sig")
